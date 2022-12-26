bbae774cac5728be703b56eecd60cab5f8c96deb
shimo8810 <shimo.8810@gmail.com>
2022-12-26 20:00:26 +0900

    [update] run関数をテスト可能に変更

--- main.go
+++ main.go
@@ -1,19 +1,43 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+
+	"golang.org/x/sync/errgroup"
 )
 
-func main() {
-	err := http.ListenAndServe(
-		":18080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
-	)
+	}
 
-	if err != nil {
-		fmt.Printf("failed to terminate server: %v", err)
+	eg, ctx := errgroup.WithContext(ctx)
+
+	//  別のgoroutineでHTTP Server を起動する
+	eg.Go(func() error {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("failed toclose %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown %+v", err)
+	}
+
+	return eg.Wait()
+}
+
+func main() {
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server %v", err)
 	}
 }

e11367f1aa08b3ef48edcd85ad21d6f3ddc00272
shimo8810 <shimo.8810@gmail.com>
2022-12-28 23:06:35 +0900

    [update] ルーティング定義を分離

--- main.go
+++ main.go
@@ -5,18 +5,11 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/shimo8810/go_todo_app/config"
-	"golang.org/x/sync/errgroup"
 )
 
 func run(ctx context.Context) error {
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -24,37 +17,14 @@ func run(ctx context.Context) error {
 
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
 	if err != nil {
-		log.Fatalf("failed to listen port: %d: %v", cfg.Port, err)
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
 	}
 
 	url := fmt.Sprintf("http://%s", l.Addr().String())
-
-	log.Printf("start with :%v", url)
-
-	s := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// time.Sleep(5 * time.Second)
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
-
-	eg, ctx := errgroup.WithContext(ctx)
-
-	//  別のgoroutineでHTTP Server を起動する
-	eg.Go(func() error {
-		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
-			log.Printf("failed toclose %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown %+v", err)
-	}
-
-	return eg.Wait()
+	log.Printf("start with: %v", url)
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
 
 func main() {
